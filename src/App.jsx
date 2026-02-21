05ea017e81eebb78205bb5280f2fc7be7276451e
deborah-shaw <sendmoreinfo2000@yahoo.com>
2026-02-21 21:15:02 +0800

    Update project order

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home.jsx'
 import About from './pages/about.jsx'
+import HeadsInTheCloud from './pages/headsInTheCloud.jsx'
 import Cst300 from './pages/cst300.jsx'
 import Cst338 from './pages/cst338.jsx'
 import Cst363 from './pages/cst363.jsx'
@@ -11,12 +12,8 @@ import Cst383 from './pages/cst383.jsx'
 import Cst489 from './pages/cst489.jsx'
 import Cst499 from './pages/cst499.jsx'
 import Cst334 from './pages/cst334.jsx'
-// import Cst370 from './pages/cst370.jsx'
-// import Cst438 from './pages/cst438.jsx'
-// import Cst328 from './pages/cst328.jsx'
-// import Cst329 from './pages/cst329.jsx'
 import Contact from './pages/contact.jsx'
-import HeadsInTheCloud from './pages/projectHeads.jsx'
+
 
 function App() {
 
@@ -25,6 +22,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>} />
+            <Route path="/headsInTheCloud" element={<HeadsInTheCloud/>} />
             <Route path="/cst300" element={<Cst300/>} />
             <Route path="/cst338" element={<Cst338/>} />
             <Route path="/cst363" element={<Cst363/>} />
@@ -35,12 +33,7 @@ function App() {
             <Route path="/cst489" element={<Cst489/>} />
             <Route path="/cst499" element={<Cst499/>} />
             <Route path="/cst334" element={<Cst334/>} />
-            {/* <Route path="/cst370" element={<Cst370/>} />
-            <Route path="/cst438" element={<Cst438/>} />
-            <Route path="/cst328" element={<Cst328/>} />
-            <Route path="/cst329" element={<Cst329/>} /> */}
             <Route path="/contact" element={<Contact/>} />
-            <Route path="/projectHeads" element={<HeadsInTheCloud/>} />
         </Routes>
     </Router>
   )
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -9,8 +9,7 @@ function Nav(){
     const coursePaths = [
         "/cst300", "/cst338", "/cst363", "/cst311", "/cst336",
         "/cst462", "/cst383", "/cst489", "/cst499", "/cst334",
-        // "/cst370", "/cst438", "/cst328", "/cst329", 
-        "/projectHeads"
+        "/headsInTheCloud"
     ];
     const isCourseSelected = coursePaths.includes(location.pathname);
 
@@ -43,6 +42,7 @@ function Nav(){
                             borderRadius: '10px', // This makes the corners rounded
                         }}
                     >
+                        <Link to="/headsInTheCloud" className={location.pathname === "/headsInTheCloud" ? "current" : ""}>Heads in the Cloud - RDF</Link><br/>
                         <Link to="/cst300" className={location.pathname === "/cst300" ? "current" : ""}>CST300 - Major ProSeminar</Link><br/>
                         <Link to="/cst338" className={location.pathname === "/cst338" ? "current" : ""}>CST338 - Software Design</Link><br/>
                         <Link to="/cst363" className={location.pathname === "/cst363" ? "current" : ""}>CST363 - Introduction to Database Systems</Link><br/>
@@ -53,11 +53,6 @@ function Nav(){
                         <Link to="/cst489" className={location.pathname === "/cst489" ? "current" : ""}>CST489 - Capstone Project Planning</Link><br/>
                         <Link to="/cst499" className={location.pathname === "/cst499" ? "current" : ""}>CST499 - Computer Science Capstone</Link><br/>
                         <Link to="/cst334" className={location.pathname === "/cst334" ? "current" : ""}>CST334 - Operating Systems</Link><br/>
-                        {/* <Link to="/cst370" className={location.pathname === "/cst370" ? "current" : ""}>CST370 - Design and Analysis of Algorithms</Link><br/>
-                        <Link to="/cst438" className={location.pathname === "/cst438" ? "current" : ""}>CST438 - Software Engineering</Link><br/>
-                        <Link to="/cst328" className={location.pathname === "/cst328" ? "current" : ""}>CST328 - Digital Art and Design</Link><br/>
-                        <Link to="/cst329" className={location.pathname === "/cst329" ? "current" : ""}>CST329 - Reasoning with Logic</Link><br/> */}
-                        <Link to="/projectHeads" className={location.pathname === "/projectHeads" ? "current" : ""}>Heads in the Cloud - RDF</Link><br/>
                     </div>
                 )}
             </div>
