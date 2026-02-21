d8e6255c65074a30219dee45d1fbf0860d40c7c6
deborah-shaw <sendmoreinfo2000@yahoo.com>
2026-02-21 20:25:20 +0800

    Change logo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ function Footer(){
             <hr/>
             Deborah's Personal Website &copy;{new Date().getFullYear()}
             <figure>
-                <img src="/Personal-Website-2/img/logo-brain.jpg" alt="Logo of Smart Brain" style={{ maxWidth: "100px", maxHeight: "100px", borderRadius: "12px" }} />
+                <img src="/Personal-Website-2/img/logo-ds.jpg" alt="Logo of Smart Brain" style={{ maxWidth: "100px", maxHeight: "100px", borderRadius: "12px" }} />
             </figure>
         </footer>
     );
