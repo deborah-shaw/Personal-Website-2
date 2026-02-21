3adc6466afdebdb3b1a7dece34e13bdd1f8f35b2
deborah-shaw <sendmoreinfo2000@yahoo.com>
2026-02-21 18:38:47 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,6 @@ function Footer(){
         <footer>
             <hr/>
             Deborah's Personal Website &copy;{new Date().getFullYear()}
-            <br/><br/>
             <figure>
                 <img src="/Personal-Website-2/img/logo-brain.jpg" alt="Logo of Smart Brain" style={{ maxWidth: "100px", maxHeight: "100px", borderRadius: "12px" }} />
             </figure>
